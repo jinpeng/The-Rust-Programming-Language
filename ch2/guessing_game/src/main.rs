248270c2b4f0fbdff2c1ef940d3b72445531913a
jinpeng <jinpeng.dong@gmail.com>
2021-02-18 16:21:50 +0800

    ch2 guessing_game

--- ch2/guessing_game/src/main.rs
+++ ch2/guessing_game/src/main.rs
@@ -5,14 +5,17 @@ use std::io;
 fn main() {
     println!("Guessing the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("Secret number is {}", secret_number);
+    // println!("Secret number is {}", secret_number);
     loop {
         println!("Input your guess:");
         let mut guess_str = String::new();
-        io::stdin()
-            .read_line(&mut guess_str)
-            .expect("Failed to read line");
-        println!("Your guessed: {}", &guess_str);
+        match io::stdin().read_line(&mut guess_str) {
+            Ok(x) => x,
+            Err(_) => {
+                println!("Failed to read line from standard input.");
+                continue;
+            }
+        };
         let guess: u32 = match guess_str.trim().parse() {
             Ok(num) => num,
             Err(_) => {
@@ -20,6 +23,7 @@ fn main() {
                 continue;
             }
         };
+        println!("Your guessed: {}", guess);
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
